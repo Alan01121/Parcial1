0a07bdd1679d0b958fd0b1de185a38143bc1afd5
Alan <100720001+Alan01121@users.noreply.github.com>
2022-03-15 09:51:14 -0600

    Programa en python
    Programa de suma entre dos numeros.

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Ingrese un numero')
+primerNumero = int(input())
+
+print('Ingrese otro numero')
+segundoNumero = int(input())
+
+operacion = primerNumero + segundoNumero
+
+print('El resultado de la suma es:', operacion)
